ac47247c003f1792c450efa683b6fbe24cc10220
Filipe Alves <contato@filipealves.com.br>
2018-09-17 18:27:17 -0300

    tests: add heap sort test

---
+++ src/tests/heap_sort.c
@@ -0,0 +1,33 @@
+/**    _                _ _                   ___             _      _
+ *    / \   _ __   __ _| (_)___  ___    ___  |  _ \ _ __ ___ (_) ___| |_ ___
+ *   / _ \ | '_ \ / _` | | / __|/ _ \  / _ \ | |_) | '__/ _ \| |/ _ \ __/ _ \
+ *  / ___ \| | | | (_| | | \__ \  __/ |  __/ |  __/| | | (_) | |  __/ |  (_) |
+ * /_/   \_\_| |_|\__,_|_|_|___/\___|  \___| |_|   |_|  \___// |\___|\__\___/
+ *                                                         |__/
+ *      _           _    _                  _ _
+ *   __| | ___     / \  | | __ _  ___  _ __(_) |_ _ __ ___   ___  ___
+ *  / _` |/ _ \   / _ \ | |/ _` |/ _ \| '__| | __| '_ ` _ \ / _ \/ __|
+ * | (_| |  __/  / ___ \| | (_| | (_) | |  | | |_| | | | | | (_) \__ \
+ *  \__,_|\___| /_/   \_\_|\__, |\___/|_|  |_|\__|_| |_| |_|\___/|___/
+ *                         |___/
+ *
+ * Autor: Filipe Alves de Fernando
+ * Professor: Daniel Carlos Guimarães Pedronette
+ * Disiciplina: Análise e Projeto de Algoritmos
+ * Programa de Pós Graduação em Ciência da Computação
+ *
+ */
+
+#include <sort.h>
+#include <vector.h>
+
+int main(int argc, char *argv[])
+{
+    int len = 20;
+    vec_t v = create_vec(len, 10);
+    print_vec(v);
+    heap_sort(v);
+    print_vec(v);
+    destroy_vec(v);
+    return 0;
+}
